ef77b3c2cfe098f88831e9dffe619dc4d0b965b9
Алина Жидких <coo.alinazh@yandex.ru>
2022-10-10 18:13:51 +0300

    добавили решенные задачи

---
+++ task21/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int x1 = Coord("Введите координату X первой точки: ");
+int y1 = Coord("Введите координату Y первой точки: ");
+int z1 = Coord("Введите координату Z первой точки: ");
+int x2 = Coord("Введите координату X второй точки: ");
+int y2 = Coord("Введите координату Y второй точки: ");
+int z2 = Coord("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+int Coord(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
---
+++ task23/Program.cs
@@ -0,0 +1,21 @@
+﻿;/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void GetNumb (int number)
+{
+Console.Write ($"{number} ->");
+
+    for (int i = 1; i <= number; i++)
+{
+double cube = Math.Pow(i,3);
+Console.Write($"{cube}, ");
+}
+}
+
+Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+
+GetNumb(numb);
